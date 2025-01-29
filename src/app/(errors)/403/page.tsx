3c7acf52c1f26fd84c6ca366d8e41949072f0dab
whysofail <adam.satria.armansyah@gmail.com>
2025-01-29 16:01:41 +0700

    feat: show forbidden or page not found instead of forbidden in page /403

--- src/app/(errors)/403/page.tsx
+++ src/app/(errors)/403/page.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
-import { ShieldX } from "lucide-react";
-
+import { ShieldX, FileWarning } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-const Forbidden = () => {
+const ForbiddenOrNotFound = () => {
   return (
     <main className="flex min-h-[calc(100vh-theme(spacing.16))] flex-col items-center justify-center gap-6 py-16 text-center">
       <div className="flex flex-col items-center gap-2">
-        <div className="bg-muted rounded-full p-4">
-          <ShieldX className="text-muted-foreground h-8 w-8" />
+        <div className="bg-muted flex items-center justify-center rounded-full p-4">
+          <ShieldX className="text-muted-foreground h-8 w-8" />|
+          <FileWarning className="text-muted-foreground h-8 w-8" />
         </div>
         <h1 className="text-2xl font-semibold tracking-tight">
-          Access Forbidden
+          Access Denied or Page Not Found
         </h1>
         <p className="text-muted-foreground text-sm">
-          You don&apos;t have permission to access this resource.
+          You either don&apos;t have permission to access this page, or it
+          doesn&apos;t exist.
         </p>
       </div>
       <Button asChild>
@@ -24,4 +25,4 @@ const Forbidden = () => {
   );
 };
 
-export default Forbidden;
+export default ForbiddenOrNotFound;
